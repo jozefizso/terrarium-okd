1696bc723c76bfa1401bdc1affdc106ea56487e7
Jozef Izso <jozef.izso@gmail.com>
2019-05-06 20:10:07 +0200

    Prototype for configuring vm with cloud-init

--- main.tf
+++ main.tf
@@ -17,21 +17,51 @@ provider "esxi" {
 # 14 - ESXi 6.7 (Workstation Pro 14.x)
 # 13 - ESXi 6.5
 
-resource "esxi_guest" "centos01" {
-  guest_name = "centos01"
-  disk_store = "datastore1"
-  notes = "Terraform virtual machine."
+#########################################
+#  cloud-init for vmware!
+#  You must install it on your source VM before cloning it!
+#    See https://github.com/akutz/cloud-init-vmware-guestinfo
+#########################################
 
-  memsize = 1024 # MB
-  numvcpus = 2
+
+#
+# Template for initial configuration bash script
+#    template_file is a great way to pass variables to
+#    cloud-init
+data "template_file" "Default" {
+  template = "${file("userdata.tmpl")}"
+  vars = {
+    HOSTNAME = "okd-master"
+    HELLO    = "Hello World!"
+  }
+}
+
+resource "esxi_guest" "okd-master" {
+  guest_name = "okd-master"
+  disk_store = "datastore1"
+  notes = "OpenShift master machine."
+
+  memsize = 4096 # MB
+  numvcpus = 4
 
   virthwver = 14
-  power = "off"
+  power = "on"
   guest_startup_timeout = 10
+
+  clone_from_vm = "_centos-7.6.1810.03_template"
 
   network_interfaces = [
     {
       virtual_network = "public_network"
     },
   ]
+
+  # cloudinit data for vm
+  #
+  # Troubleshoot cloud-init data:
+  #  vmtoolsd --cmd "info-get guestinfo.userdata"
+  guestinfo = {
+    userdata.encoding = "gzip+base64"
+    userdata = "${base64gzip(data.template_file.Default.rendered)}"
+  }
 }
